415b5630588f0878f7b6765b53a60a8a9e523180
DucAnhCao11 <ducanhcao2611@gmail.com>
2025-05-28 14:04:06 +0700

    update xử lý phụ phí

--- src/main/java/com/java/project/services/HoaDonService.java
+++ src/main/java/com/java/project/services/HoaDonService.java
@@ -176,8 +176,11 @@ public class HoaDonService {
             hoaDon.setPhuPhi(BigDecimal.ZERO);
         }
         BigDecimal chenhLech = BigDecimal.valueOf(thongTinDonHangRequest.getPhiShip() - hoaDon.getPhiShip());
-        hoaDon.setTongTien(hoaDon.getTongTien() + chenhLech.doubleValue());
+//        hoaDon.setTongTien(hoaDon.getTongTien() + chenhLech.doubleValue());
         hoaDon.setPhiShip(thongTinDonHangRequest.getPhiShip());
+
+        BigDecimal phuPhi = hoaDon.getPhuPhi() != null ? hoaDon.getPhuPhi() : BigDecimal.ZERO;
+        hoaDon.setPhuPhi(phuPhi.add(chenhLech));
 
         return hoaDonMapper.toHoaDonResponse(hoaDonRepository.save(hoaDon));
     }
